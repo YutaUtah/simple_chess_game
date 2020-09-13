e1c13456cfec0622f760cf11c99f84daa485d822
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2020-09-13 21:32:27 +0900

    Initial commit

---
+++ checkers/__init__.py
@@ -0,0 +1,4 @@
+"Initialize this folder: checkers as a package"
+# In main.py, as soon as checkers is called, __init__.py inside of checkers folder will run
+from .config import *
+from .board import *
